ebb0887036aded582b315ed49808f921eef497cd
JoseAngelGuerreroInzunza <158419935+JoseAngelGuerreroInzunza@users.noreply.github.com>
2024-03-12 22:22:29 -0700

    Add files via upload

---
+++ promedio/Principa.java
@@ -0,0 +1,9 @@
+package promedio;
+
+public class Principa {
+    public static void main(String [] arg){
+        Promedio objPromedio = new Promedio();
+        objPromedio.calcularPromedio();
+    }
+    
+}
